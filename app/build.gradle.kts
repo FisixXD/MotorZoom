9592dfdc893d92206d2542d73508a912d003e1b5
FisixXD <fisixxd@gmail.com>
2026-08-07 01:00:25 -0300

    Atualizar app/build.gradle.kts para VHS 60

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "app.motorzoom"
         minSdk = 26
         targetSdk = 35
-        versionCode = 13
-        versionName = "0.6.4"
+        versionCode = 14
+        versionName = "0.7.0"
     }
 
     buildFeatures {
