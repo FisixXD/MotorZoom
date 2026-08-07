980aab0abc283a2ed3fadc517b357eee8d1eaee8
FisixXD <fisixxd@gmail.com>
2026-08-07 01:18:03 -0300

    Forçar 60 fps no modo VHS (app/build.gradle.kts)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "app.motorzoom"
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
-        versionName = "0.7.0"
+        versionCode = 15
+        versionName = "0.7.1"
     }
 
     buildFeatures {
