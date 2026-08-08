041b6292e7e4b8d8b9ebefc836271017f7999aad
FisixXD <fisixxd@gmail.com>
2026-08-07 22:16:12 -0300

    Release MotorZoom 0.12.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "app.motorzoom"
         minSdk = 26
         targetSdk = 35
-        versionCode = 21
-        versionName = "0.11.1"
+        versionCode = 22
+        versionName = "0.12.0"
     }
 
     buildFeatures {
