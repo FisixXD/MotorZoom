dbd6ceb0fcebbb9e71ccae0d4bd45711ad8c287e
FisixXD <fisixxd@gmail.com>
2026-08-14 00:30:32 -0300

    Add preset native API

--- app/src/main/java/app/motorzoom/NativeNtsc.kt
+++ app/src/main/java/app/motorzoom/NativeNtsc.kt
@@ -4,6 +4,8 @@ object NativeNtsc {
     init { System.loadLibrary("ntsc_android") }
 
     external fun configure(presetJson: String): Boolean
+    external fun defaultPreset(): String
+    external fun normalizePreset(presetJson: String): String?
     external fun processRgba(
         pixels: ByteArray,
         width: Int,
